1956ea241697da36525da5b3638029f737f8fdca
Henrique Pegacha <A44003@alunos.isel.pt>
2018-12-25 16:25:30 +0000

    useless commit message here

--- Ficha6EDA/main.cpp
+++ Ficha6EDA/main.cpp
@@ -5,21 +5,43 @@
 #define AZUL RGB(0,0,255)
 #define VERMELHO RGB(255,0,0)
 int main() {
-
+	//Criação de janela
 	Janela janela;
 	HWND janelaId;
-
 	if (janela.Criar("Mapa Lisboa e arredores","MAPA.bmp")) {
 		if ((janelaId = janela.ObterId()) != NULL) {
 			
 			MSG msg;
 			while (GetMessage(&msg, 0, 0, 0)) {
 				DispatchMessage(&msg);
+				
 
-				Ponto point(200, 100);
+				FILE *ficheiro;
+				ficheiro = fopen("Mapa.txt", "r");
+				if (ficheiro == NULL)
+					return false;
+				else {
+					int npontos;
+					fscanf(ficheiro, "%d", &npontos);
+					Ponto* coords;
+					Circulo* local;
+					coords = new Ponto[npontos];
+					local = new Circulo[npontos];
+					int xcoord, ycoord;
+					for (int i = 0; i < npontos; i++) {
+						fscanf(ficheiro, "%d, %d", &xcoord, &ycoord);
+						coords[i] = Ponto(xcoord, ycoord);
+						local[i] = Circulo(coords[i],10,i+1);
+						local[i].desenhar(janelaId, VERDE);
 
-				Circulo circ(point,20,22);
-				circ.desenhar(janelaId, VERMELHO);
+					}
+				}
+
+
+
+
+
+
 
 				if (janela.Click()) {
 					
